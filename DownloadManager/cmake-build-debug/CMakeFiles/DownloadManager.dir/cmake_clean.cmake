b3829f8e851938869b5ba844254f332878371649
JanHaladej <115941416+JanHaladej@users.noreply.github.com>
2022-12-29 22:05:49 +0100

    Start Hello World

---
+++ DownloadManager/cmake-build-debug/CMakeFiles/DownloadManager.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DownloadManager.dir/main.cpp.o"
+  "DownloadManager"
+  "DownloadManager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DownloadManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
